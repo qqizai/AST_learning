8beb76bf551803558923b79a5638afba46f1a44c
qqizai <19960112LdZ>
2020-09-13 18:01:11 +0800

    还原对象的取值赋值

--- sojson/just_do_IT/test.js
+++ sojson/just_do_IT/test.js
@@ -4,53 +4,75 @@ const generator = require("@babel/generator").default
 const types = require("@babel/types")
 
 
-var a = {
-    'abc': function(arg1, arg2){
-        return arg1+arg2;
-    },
-    'bcd': function test (arg1, arg2){
-        return arg1+arg2;
+/********************************************************************
+ 处理FunctionDeclaration
+ 思路:对于实参全部是字面量的函数调用，且运行结果唯一时，可以进行替换
+ before:
+ function add(a,b)
+ {
+ return a+b;
+}
+
+ s = add(1,2) + add(111,222);
+
+ after:
+
+ function add(a, b) {
+  return a + b;
+}
+
+ s = 3 + 333;
+ ********************************************************************/
+
+const visitor = {
+    FunctionDeclaration(path) {
+        let {
+            id
+        } = path.node;
+        const binding = path.scope.parent.getBinding(id.name);
+        if (!binding || binding.constantViolations.length > 0) {
+            return;
+        }
+        let refer_paths = binding.referencePaths;
+        if (refer_paths == 0) {
+            path.remove();
+            return;
+        }
+
+        let params = path.get('params').toString();
+        let code = path.get('body').toString();
+        if (code.indexOf("try") != -1 || code.indexOf('random') != -1 || code.indexOf('Date') != -1) { //返回值不唯一不做处理
+            return;
+        }
+        var call_fun = new Function(params, code);
+        for (const refer_path of binding.referencePaths) {
+            let call_express = refer_path.findParent(p => p.isCallExpression());
+            if (!call_express) continue;
+            let args = call_express.get('arguments');
+            if (args.length == 0 || !args.every(el => el.isLiteral())) return;
+
+            for (let i in args) {
+                args[i] = args[i].node.value;
+            }
+
+            try {
+                let value = call_fun.apply(null, args);
+                value != undefined && call_express.replaceWith(types.valueToNode(value));
+            } catch (e) {};
+        }
     },
 }
 
+let code = " function add(a,b)\n" +
+    " {\n" +
+    " return a+b;\n" +
+    "}\n" +
+    "\n" +
+    " s = add(1,2) + add(111,222);"
 
-/*console.log(a['abc'](12, 23))
-console.log(a['bcd'](12, 23))
+let ast = parser.parse(code)
 
+// traverse(ast, visitor)
 
-;(function test (arg1, arg2){
-    console.log(arg1+arg2)
-    return arg1+arg2;
-})(12, 23)*/
-
-
-// const code = "var a = b + 2; var b = 4;var c = 'aaa'; function test(a,b){return a+b;}";
-const code = "function test(a,b){return a+b;}";
-
-var ast = parser.parse(code)
-
-traverse(ast, {
-    enter: function (path){
-        var res = path.call("test", 12, 23);
-        console.log(path.scope)
-        console.log(res)
-
-    }
-})
-
-/*
-var a = b + 2; var b = 4
-
-console.log(a)
-console.log(b)
-
-var test_add = function(a, b){
-    return a+b;
-}
-
-var result = test_add(12, 23);
-console.log(result)
-*/
-
-
+console.log(generator(ast).code)
 
--- tools/utils_object_member.js
+++ tools/utils_object_member.js
@@ -1,9 +1,58 @@
-/**
- * 本脚本是处理 MemberExpression，即(对象的)成员表达式
- *
- * 注意：可能很多不同的类型，如果遇到了新的，可以自己扩展进来
- *
- * */
+/****************************************************************************************************************
+
+ 本脚本是处理对象的取值、赋值情况
+
+  注意：可能很多不同的类型，如果遇到了新的，可以自己扩展进来
+  (对象属性的值是函数的时候，对于实参全部是字面量的函数调用，且运行结果唯一时，才可以进行替换)
+
+
+ before：
+
+ function f() {
+    var a = {
+        'info': 123,
+        'bc': 456,
+        'dd': function test(abc, abd) {
+            // return typeof abc + "---" + abd;
+            return abc + abd;
+        },
+        age: 10,
+        addr: "这是哪里",
+        nb: false
+    };
+    var abc = a['info'];
+    abc = a.info;
+    var abd = a.bc;
+    var dd = a.dd(12,23) + a.dd(11,22);
+    var nb = a.nb;
+
+}
+
+
+ ------------------------------------------------------------------------------------------
+ affter:
+
+ function f() {
+    var a = {
+        'info': 123,
+        'bc': 456,
+        'dd': function test(abc, abd) {
+            // return typeof abc + "---" + abd;
+            return abc + abd;
+        },
+        age: 10,
+        addr: "这是哪里",
+        nb: false
+    };
+    var abc = 123;
+    abc = 123;
+    var abd = 456;
+    var dd = 35 + 33;
+    var nb = false;
+
+}
+
+ ***************************************************************************************************************/
 
 const parser = require("@babel/parser")
 const traverse = require("@babel/traverse").default
@@ -12,27 +61,20 @@ const types = require("@babel/types")
 
 
 
-/**
- * 类型1
- *
- * 下面我举个例子，比如：
- * var test_obj = {a: 123, b:456};
- * var member_value = test_obj["a"];
- *
- * 将 test_obj["a"] 直接换成 123，所以 var member_value = 123;
- * */
 
-let visitor_obj = {
+const visitor_obj = {
     VariableDeclarator: function (path) {
         var node = path.node;
 
-        //如果不是对象表达式，不用处理，直接跳过
         if (!types.isObjectExpression(node.init)) {
+            //如果不是对象表达式，不用处理，直接跳过
             return;
         }
 
         let obj_name = node.id.name;
 
+
+        //=========还原对象的赋值===========================================================
         //1、首先查看当前节点下，是否有赋值的情况，有的话，直接将 obj_name.info = 123 替换为：往 obj_name 的属性list里面增加一个ObjectProperty
         let all_next_siblings = path.parentPath.getAllNextSiblings();
         all_next_siblings.forEach(next_sibling => {
@@ -49,36 +91,18 @@ let visitor_obj = {
             let { left, operator, right } = expression.node;
             if (left.type === "MemberExpression" && left.object.name === obj_name && operator === "=") {
                 //开始往 obj_name 的属性list里面增加一个ObjectProperty
-
-                //方法一
-                /*//这个值的类型可能有很多种的，暂时还不知道如何覆盖全部/绝大部分，目前这里只是兼顾了 StringLiteral、NumericLiteral
-                if (!(right.type === "StringLiteral" || right.type === "NumericLiteral")) return;
-                let tmp_objectProperty = {
-                    type: "ObjectProperty",
-                    key: {
-                        type: "StringLiteral",
-                        value: left.property.name
-                    },
-                    value: {
-                        type: right.type,
-                        value: right.value
-                    }
-                }
-                node.init.properties.push(tmp_objectProperty);*/
-
-                //方法二：应该是可以覆盖全部的(只测试了 StringLiteral、NumericLiteral、UnaryExpression、FunctionExpression)
                 node.init.properties.push(types.ObjectProperty(left.property, right));
-
                 //然后移除当前已经处理的节点，一定记得需要检验一下，最后的代码，是否符合
                 next_sibling.remove();
             }
-
         })
+        //====================================================================
 
 
+        //=========还原从对象取值===========================================================
         //2、对对象名为 obj_name 的属性进行遍历，从这个节点的父节点开始往下找，如果找到了有相关应用的话，那么需要还原回去
         var objPropertiesList = node.init.properties;
-        if (objPropertiesList.length == 0) return;
+        if (objPropertiesList.length === 0) return;
 
         objPropertiesList.forEach(prop => {
             //判断是否为 NumericLiteral、StringLiteral、简单的 FunctionExpression (直接返回的)
@@ -90,18 +114,18 @@ let visitor_obj = {
             //存在情况
             //如 var abc = a.info 或 var abc = a['info']
             //同时还得再细分 CallExpression,如 var res = a['dd'](12, 23) 或者 res = a['dd'](12, 23)
-            if (prop.key.type === "NumericLiteral" || prop.key.type === "StringLiteral") {
-
+            //=========还原简单的字面量===========================================================
+            if (types.isLiteral(prop.value)){
                 fnPath.traverse(
                     {
                         //var abc = a.info、var abc = a['info']、abc = a.info、abc = a['info']
                         MemberExpression: function (_path) {
                             var _node = _path.node
+
                             if (_node.object.name === obj_name
                                 && (_node.property.value === obj_key_name || _node.property.name === obj_key_name)
                                 && _path.parent.type !== "CallExpression"
                             ) {
-                                console.log(_path.parent.type)
                                 _path.replaceWith(prop.value);
                             }
 
@@ -109,8 +133,51 @@ let visitor_obj = {
                     }
                 )
             }
+            //====================================================================
 
-            if (prop.type === "CallExpression") {
+
+            //=========还原简单的函数调用===========================================================
+            if (types.isFunctionExpression(prop.value) && types.isReturnStatement(prop.value.body.body[0])) {
+                fnPath.traverse(
+                    {
+                        CallExpression: function (_path) {
+                            var _node = _path.node
+                            var _callee = _node.callee
+
+                            var _code = generator(prop.value.body).code
+                            //返回值不唯一不做处理
+                            if (_code.indexOf("try") !== -1 || _code.indexOf('random') !== -1 || _code.indexOf('Date') !== -1) {
+                                return;
+                            }
+
+                            var params = [];
+                            if (prop.value.params.length === 0) return;
+                            for (let i in prop.value.params) {
+                                params.push(prop.value.params[i].name);
+                            }
+
+                            var params_str = params.join(",");
+                            var call_fun = new Function(params_str, _code);
+                            var args = _node.arguments;
+                            for (let i in args) {
+                                args[i] = args[i].value;
+                            }
+
+                            if(types.isMemberExpression(_callee) && _callee.object.name === obj_name && (_callee.property.value === obj_key_name || _callee.property.name === obj_key_name)) {
+                                try {
+                                    let value = call_fun.apply(null, args);
+                                    if (value !== undefined){
+                                        _path.replaceWith(types.valueToNode(value));
+                                    }
+                                } catch (e) {
+                                }
+
+                            }
+
+                        }
+                    }
+                )
+
 
             }
 
@@ -121,95 +188,39 @@ let visitor_obj = {
     }
 }
 
-
-
 function handle_obj_member_value(ast) {
     traverse(ast, visitor_obj);
     return ast
-
 }
 
 
-var code = "function f() {\n" +
-    "    var a = {\n" +
-    "        'info': 123,\n" +
-    "        'bc': 456,\n" +
-    "        'dd': function test(abc, abd){\n" +
-    "            return abc+abd;\n" +
-    "        }\n" +
-    "    }\n" +
-    "\n" +
-    "\n" +
-    "    a.age = 10\n" +
-    "    a.addr = \"这是哪里\"\n" +
-    "    a.test = !(function(abc, abd){\n" +
-    "        return abc+abd;\n" +
-    "    })(1213, 456);\n" +
-    "\n" +
-    "    var abc = a.info\n" +
-    "    abc = a.info\n" +
-    "    var abd = a['info']\n" +
-    "    var abd = 1213\n" +
-    "\n" +
-    "    var res = a['dd'](12, 23)\n" +
-    "}"
+function example(){
+    var code = "function f() {\n" +
+        "    var a = {\n" +
+        "        'info': 123,\n" +
+        "        'bc': 456,\n" +
+        "        'dd': function test(abc, abd) {\n" +
+        "            return typeof abc + \"---\" + abd;\n" +
+        "        },\n" +
+        "        age: 10,\n" +
+        "        addr: \"这是哪里\",\n" +
+        "        nb: false\n" +
+        "    };\n" +
+        "    var abc = a['info'];\n" +
+        "    abc = a.info;\n" +
+        "    var abd = a.bc;\n" +
+        "    var dd = a.dd(12,23);\n" +
+        "    var nb = a.nb;\n" +
+        "\n" +
+        "}"
 
-var ast = parser.parse(code)
-ast = handle_obj_member_value(ast)
-console.log(generator(ast).code)
+    var ast = parser.parse(code)
+    ast = handle_obj_member_value(ast)
 
-/*
-function f() {
-    var a = {
-        'info': 123,
-        'bc': 456,
-        'dd': function(abc, abd){
-            return abc+abd;
-        }
-    }
-
-
-    a.age = 10
-    a.addr = "这是哪里"
-    a.test = !(function(abc, abd){
-        return abc+abd;
-    })(1213, 456);
-
-    var abc = a.info
-    abc = a.info
-    var abd = a['info']
-    var abd = 1213
-
-    var res = a['dd'](12, 23)
-}
-*/
-
-
-
-function f() {
-    var a = {
-        'info': 123,
-        'bc': 456,
-        'dd': function test(abc, abd) {
-            return abc + abd;
-        },
-        age: 10,
-        addr: "这是哪里",
-        test: !function (abc, abd) {
-            return abc + abd;
-        }(1213, 456)
-    };
-    var abc = 123;
-    abc = 123;
-    var abd = 123;
-    var abd = 1213;
-
-    var res = function test(abc, abd) {
-        return abc + abd;
-    }(12, 23);
-
-    console.log(res)
+    console.log(generator(ast).code)
 }
 
-f()
 
+module.exports = {
+    "handle_obj_member_value": handle_obj_member_value
+}
